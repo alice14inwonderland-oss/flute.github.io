9de13235ff384d0aa3c947d73134670e1e296e3f
はやあしうさぎ <alice14inwonderland@gmail.com>
2025-11-25 10:34:39 +0900

    Add files via upload

---
+++ composers/composer_list.js
@@ -0,0 +1,188 @@
+// composerList.js
+// 作曲家200名（バロック〜現代・フルート作品に関連）
+
+const composerList = [
+  // まずはデータの土台として少数を置いています。
+  // この後ユーザーの要望に合わせて 200 名のフルリストを追加します。
+
+ {
+  "id": "telemann",
+  "name": "ゲオルク・フィリップ・テレマン",
+  "name_en": "Georg Philipp Telemann",
+  "era": "Baroque",
+  "kana": "てれまん",
+  "birth": 1681,
+  "death": 1767,
+  "works": [
+    "無伴奏フルートのための12の幻想曲",
+    "フルートソナタ F-dur",
+    "組曲 a-moll（フルート協奏曲として演奏される）"
+  ]
+},
+
+  {
+    "id": "bach_js",
+    "name": "ヨハン・セバスティアン・バッハ",
+    "name_en": "Johann Sebastian Bach",
+    "era": "Baroque",
+    "kana": "ばっは",
+    "birth": 1685,
+    "death": 1750,
+    "works": [
+      "フルートソナタ BWV1030",
+      "フルートソナタ BWV1031",
+      "フルートソナタ BWV1032",
+      "フルートソナタ BWV1033",
+      "フルートソナタ BWV1034",
+      "フルートソナタ BWV1035",
+      "管弦楽組曲第2番 BWV1067"
+    ]
+  },
+  {
+    "id": "bach_cpe",
+    "name": "カール・フィリップ・エマヌエル・バッハ",
+    "name_en": "Carl Philipp Emanuel Bach",
+    "era": "Baroque",
+    "kana": "しーぴーいーばっは",
+    "birth": 1714,
+    "death": 1788,
+    "works": [
+      "フルート協奏曲 ニ短調 H.425",
+      "フルート協奏曲 ト長調 H.445",
+      "フルート協奏曲 イ長調 H.438"
+    ]
+  },
+  {
+    "id": "bach_wf",
+    "name": "ヴィルヘルム・フリーデマン・バッハ",
+    "name_en": "Wilhelm Friedemann Bach",
+    "era": "Baroque",
+    "kana": "だぶりゅーえふばっは",
+    "birth": 1710,
+    "death": 1784,
+    "works": [
+      "フルートのための6つの二重奏曲 F.54-59"
+    ]
+  },
+  {
+    "id": "telemann",
+    "name": "ゲオルク・フィリップ・テレマン",
+    "name_en": "Georg Philipp Telemann",
+    "era": "Baroque",
+    "kana": "てれまん",
+    "birth": 1681,
+    "death": 1767,
+    "works": [
+      "無伴奏フルートのための12の幻想曲",
+      "無伴奏フルートのための寓意ソナタ",
+      "フルート協奏曲 Various",
+      "組曲 a-moll TWV 55:a2"
+    ]
+  },
+  {
+    "id": "marais",
+    "name": "マラン・マレ",
+    "name_en": "Marin Marais",
+    "era": "Baroque",
+    "kana": "まれ",
+    "birth": 1656,
+    "death": 1728,
+    "works": [
+      "フルートによる編成で演奏される組曲多数",
+      "ピエール・ガブリエル校訂によるフルート版作品"
+    ]
+  },
+  {
+    "id": "couperin",
+    "name": "フランソワ・クープラン",
+    "name_en": "François Couperin",
+    "era": "Baroque",
+    "kana": "くーぷらん",
+    "birth": 1668,
+    "death": 1733,
+    "works": [
+      "王宮のコンセール（フルート版で演奏）",
+      "クラヴサン曲のフルート編"
+    ]
+  },
+  {
+    "id": "rameau",
+    "name": "ジャン＝フィリップ・ラモー",
+    "name_en": "Jean-Philippe Rameau",
+    "era": "Baroque",
+    "kana": "らもー",
+    "birth": 1683,
+    "death": 1764,
+    "works": [
+      "オペラ・バレエの多くでフルートが活躍"
+    ]
+  },
+  {
+    "id": "vivaldi",
+    "name": "アントニオ・ヴィヴァルディ",
+    "name_en": "Antonio Vivaldi",
+    "era": "Baroque",
+    "kana": "ゔぃゔぁるでぃ",
+    "birth": 1678,
+    "death": 1741,
+    "works": [
+      "フルート協奏曲 RV 433《ごしきひわ》",
+      "フルート協奏曲 RV 434",
+      "フルート協奏曲 RV 435",
+      "フルート協奏曲 RV 436",
+      "フルート協奏曲 RV 437",
+      "フルート協奏曲 RV 438",
+      "フルート協奏曲 RV 439《夜》",
+      "フルート協奏曲 RV 440"
+    ]
+  },
+  {
+    "id": "handel",
+    "name": "ゲオルク・フリードリヒ・ヘンデル",
+    "name_en": "George Frideric Handel",
+    "era": "Baroque",
+    "kana": "へんでる",
+    "birth": 1685,
+    "death": 1759,
+    "works": [
+      "フルートソナタ HWV 369",
+      "フルートソナタ HWV 374",
+      "フルートソナタ HWV 375",
+      "フルートソナタ HWV 376"
+    ]
+  },
+  {
+    "id": "blavet",
+    "name": "ミシェル・ブラヴェ",
+    "name_en": "Michel Blavet",
+    "era": "Baroque",
+    "kana": "ぶらゔぇ",
+    "birth": 1700,
+    "death": 1768,
+    "works": [
+      "フルートソナタ 作品2",
+      "フルートソナタ 作品3",
+      "フルートソナタ 作品4"
+    ]
+  },
+  {
+    "id": "boismortier",
+    "name": "ジョゼフ・ボダン・ド・ボワモルティエ",
+    "name_en": "Joseph Bodin de Boismortier",
+    "era": "Baroque",
+    "kana": "ぼわもるてぃえ",
+    "birth": 1689,
+    "death": 1755,
+    "works": [
+      "フルート協奏曲 作品15",
+      "2本のフルートのための協奏曲 作品21",
+      "フルートソナタ多数"
+    ]
+  }
+
+];
+
+export default composerList;
+
+
+
